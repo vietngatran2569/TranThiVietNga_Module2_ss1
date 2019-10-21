e1e9afddfa1532f425c30cf524389b26628fc040
vietngatran2569 <vietngatran2569@gmai.com>
2019-10-21 11:32:24 +0700

    Practice with variables and data types

---
+++ src/Practice_id2733.java
@@ -0,0 +1,17 @@
+public class Practice_id2733 {
+    public static void main(String[] args) {
+        int i=10;
+        double b=20.5;
+        float f=20.5f;
+        boolean bo=true;
+        char a='a';
+        String s="HA NOI";
+
+        System.out.println("i= "+i);
+        System.out.println("b= "+b);
+        System.out.println("f= "+f);
+        System.out.println("boolean= "+bo);
+        System.out.println("char= "+a);
+        System.out.println("string= "+s);
+    }
+}

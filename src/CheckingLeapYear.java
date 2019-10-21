bab0f3687c1bcb076e2dc66d289f47f4482a7703
vietngatran2569 <vietngatran2569@gmai.com>
2019-10-22 01:32:20 +0700

    Practice with checking leap year.

---
+++ src/CheckingLeapYear.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CheckingLeapYear {
+    public static void main(String[] args) {
+        int leapYear;
+        Scanner sc=new Scanner(System.in);
+        leapYear=sc.nextInt();
+
+        if (leapYear%4==0){
+            if (leapYear%100!=0){
+                System.out.println(leapYear+ "is leap year");
+            }else{
+                if (leapYear%400==0){
+                    System.out.println(leapYear+"is leap year");
+                }else {
+                    System.out.println(leapYear+"isn't leap year");
+                }
+            }
+        }
+    }
+}

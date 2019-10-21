e9ebd740c8cb6a3a80de3a356c247388ba6eb912
vietngatran2569 <vietngatran2569@gmai.com>
2019-10-21 11:55:22 +0700

    Practice with Linear Equation Resolver

---
+++ src/LinearEquationResolver.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class LinearEquationResolver {
+    public static void main(String[] args) {
+        float a,b;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhập vào tham số a: ");
+        a=scanner.nextFloat();
+        System.out.println("Nhập vào tham số b: ");
+        b=scanner.nextFloat();
+
+        if (a==0){
+            if (b==0){
+                System.out.println("Phương trình vô SỐ nghiệm");
+            }else{
+                System.out.println("Phương trinh vô nghiệm");
+            }
+        }else {
+            System.out.println("Nghiệm duy nhất của phương trình là :"+ (-b/a));
+        }
+    }
+}

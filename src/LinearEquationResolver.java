fb841f8fa9cf83cc98dea9c3237323de58fc229a
vietngatran2569 <vietngatran2569@gmai.com>
2019-10-21 15:19:35 +0700

    Practice with variables and data types done.

--- src/LinearEquationResolver.java
+++ src/LinearEquationResolver.java
@@ -4,6 +4,8 @@ public class LinearEquationResolver {
     public static void main(String[] args) {
         float a,b;
         Scanner scanner=new Scanner(System.in);
+        System.out.println("Bai tap giai phuong trinh bac nhat");
+        System.out.println("Phuong trinh co dang ax+b=0");
         System.out.println("Nhập vào tham số a: ");
         a=scanner.nextFloat();
         System.out.println("Nhập vào tham số b: ");
